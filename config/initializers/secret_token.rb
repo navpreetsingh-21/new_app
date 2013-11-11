0603aef13d0593126fb9f0dad8a04dec10bb12fa
rtantor-navpreet-singh <navpreet1.singh@trantorinc.com>
2013-11-11 16:50:52 +0530

    Initialize repository

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Navpreet::Application.config.secret_key_base = '66b52f45f7a68d002a7a3a6ac6e6536b85ee14df2cedf516eae735142f6b62d060a98b4145a77862b0482dc999ea1c6a72ab6cfc41160a75f779de5ba28d4a93'
