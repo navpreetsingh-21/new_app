fcd211c9551809728d436b62aec862b9294d2e85
trantor-navpreet-singh <navpreetsingh21@gmail.com>
2013-11-15 19:26:53 +0530

    Before Sign up

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,16 @@
 Navpreet::Application.routes.draw do
+  
+  root  'static_pages#home'
+  match '/help',    to: 'static_pages#help',    via: 'get'
+  match '/about',   to: 'static_pages#about',   via: 'get'
+  resources :microposts
+  resources :users
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+  
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
